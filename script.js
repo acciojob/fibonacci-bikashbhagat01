787d4c031427665567bc62dd205727ade42e3cd5
Bikash Bhagat <bikash.bhagat01@gmail.com>
2024-09-13 01:24:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 function fibonacci(num) {
     if (num === 0) return 0;
     if (num === 1) return 1;
-    
-    let a = 0, b = 1, c;
+
+    let a = 0, b = 1;
     for (let i = 2; i <= num; i++) {
-        c = a + b;
+        let c = a + b;
         a = b;
         b = c;
     }
