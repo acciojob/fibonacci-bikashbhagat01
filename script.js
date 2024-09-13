a28c51d5104c1f0620addac34fb347272762770e
Bikash Bhagat <bikash.bhagat01@gmail.com>
2024-09-13 20:30:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,40 +1,14 @@
 function fibonacci(n)
-{
-    var F = [ [ 1, 1 ], [ 1, 0 ] ];
-    if (n == 0)
-        return 0;
-        
-    power(F, n - 1);
-
-    return F[0][0];
-}
-
-function multiply(F, M)
-{
-    var x = F[0][0] * M[0][0] + F[0][1] * M[1][0];
-    var y = F[0][0] * M[0][1] + F[0][1] * M[1][1];
-    var z = F[1][0] * M[0][0] + F[1][1] * M[1][0];
-    var w = F[1][0] * M[0][1] + F[1][1] * M[1][1];
-
-    F[0][0] = x;
-    F[0][1] = y;
-    F[1][0] = z;
-    F[1][1] = w;
-}
-
-// Optimized version of power() in method 4 */
-function power(F, n) 
-{
-    if (n == 0 || n == 1)
-        return;
-        
-    var M = [ [ 1, 1 ], [ 1, 0 ] ];
-
-    power(F, n / 2);
-    multiply(F, F);
-
-    if (n % 2 != 0)
-        multiply(F, M);
+let a = 0, b = 1, c, i;
+    if( n == 0)
+        return a;
+    for(i = 2; i <= n; i++)
+    {
+    c = a + b;
+    a = b;
+    b = c;
+    }
+    return b;
 }
 
 module.exports = fibonacci;
